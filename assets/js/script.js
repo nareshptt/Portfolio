1bd27bc08fe35345aa89c418319d6acb5718c3fe
nareshpt <nareshpt253@gmail.com>
2024-07-06 20:38:41 +0530

    Solve

--- assets/js/script.js
+++ assets/js/script.js
@@ -87,10 +87,33 @@ function showSkills(skills) {
   skillsContainer.innerHTML = skillHTML;
 }
 
+fetchData().then((data) => {
+  showSkills(data);
+});
+
 // <!-- tilt js effect starts -->
 VanillaTilt.init(document.querySelectorAll(".tilt"), {
   max: 15,
 });
+
+// disable developer mode
+document.onkeydown = function (e) {
+  if (e.keyCode == 123) {
+    return false;
+  }
+  if (e.ctrlKey && e.shiftKey && e.keyCode == "I".charCodeAt(0)) {
+    return false;
+  }
+  if (e.ctrlKey && e.shiftKey && e.keyCode == "C".charCodeAt(0)) {
+    return false;
+  }
+  if (e.ctrlKey && e.shiftKey && e.keyCode == "J".charCodeAt(0)) {
+    return false;
+  }
+  if (e.ctrlKey && e.keyCode == "U".charCodeAt(0)) {
+    return false;
+  }
+};
 
 /* ===== SCROLL REVEAL ANIMATION ===== */
 const srtop = ScrollReveal({
